f5dcc8055f9f0cbabbcb6d1a996d76df00a82774
VISHAKHP10 <102618703+VISHAKHP10@users.noreply.github.com>
2024-01-23 15:52:15 +0530

    Update main.cpp

--- 4node/src/main.cpp
+++ 4node/src/main.cpp
@@ -1,22 +1,81 @@
+
 #include <EEPROM.h>
 #include <RHRouter.h>
 #include <RHMesh.h>
 #include <RH_RF95.h>
-
 #define RH_HAVE_SERIAL
 #define LED 8
 #define N_NODES 4
 
 uint8_t nodeId;
-uint8_t routes[N_NODES]; 
-int16_t rssi[N_NODES]; 
+uint8_t routes[N_NODES]; // full routing table for mesh
+int16_t rssi[N_NODES]; // signal strength info
 
+// Singleton instance of the radio driver
 RH_RF95 rf95;
 
 // Class to manage message delivery and receipt, using the driver declared above
 RHMesh *manager;
 
+// message buffer
 char buf[RH_MESH_MAX_MESSAGE_LEN];
+
+
+
+void setup() {
+  randomSeed(analogRead(0));
+  pinMode(LED, OUTPUT);
+  Serial.begin(115200);
+  while (!Serial) ; // Wait for serial port to be available
+
+  nodeId = EEPROM.read(0);
+  if (nodeId > 10) {
+    Serial.print(F("EEPROM nodeId invalid: "));
+    Serial.println(nodeId);
+    nodeId = 4;
+  }
+  Serial.print(F("initializing node "));
+
+  manager = new RHMesh(rf95, nodeId);
+
+  if (!manager->init()) {
+    Serial.println(F("init failed"));
+  } else {
+    Serial.println("done");
+  }
+  rf95.setTxPower(23, false);
+  rf95.setFrequency(915.0);
+  rf95.setCADTimeout(500);
+
+  // Possible configurations:
+  // Bw125Cr45Sf128 (the chip default)
+  // Bw500Cr45Sf128
+  // Bw31_25Cr48Sf512
+  // Bw125Cr48Sf4096
+
+  // long range configuration requires for on-air time
+  boolean longRange = false;
+  if (longRange) {
+    RH_RF95::ModemConfig modem_config = {
+      0x78, // Reg 0x1D: BW=125kHz, Coding=4/8, Header=explicit
+      0xC4, // Reg 0x1E: Spread=4096chips/symbol, CRC=enable
+      0x08  // Reg 0x26: LowDataRate=On, Agc=Off.  0x0C is LowDataRate=ON, ACG=ON
+    };
+    rf95.setModemRegisters(&modem_config);
+    if (!rf95.setModemConfig(RH_RF95::Bw125Cr48Sf4096)) {
+      Serial.println(F("set config failed"));
+    }
+  }
+
+  Serial.println("RF95 ready");
+
+  for(uint8_t n=1;n<=N_NODES;n++) {
+    routes[n-1] = 0;
+    rssi[n-1] = 0;
+  }
+
+
+}
 
 const __FlashStringHelper* getErrorString(uint8_t error) {
   switch(error) {
@@ -34,42 +93,57 @@ const __FlashStringHelper* getErrorString(uint8_t error) {
   return F("unknown");
 }
 
-void setup() {
-  randomSeed(analogRead(0));
-  pinMode(LED, OUTPUT);
-  Serial.begin(115200);
-  while (!Serial) ; 
-
-  nodeId = 1; 
-  EEPROM.write(0, nodeId);
-
-  Serial.print(F("initializing node "));
-
-  manager = new RHMesh(rf95, nodeId);
-
-  if (!manager->init()) {
-    Serial.println(F("init failed"));
-  } else {
-    Serial.println("done");
-  }
-  rf95.setTxPower(23, false);
-  rf95.setFrequency(915.0);
-  rf95.setCADTimeout(500);
-
-  Serial.println("RF95 ready");
-
+void updateRoutingTable() {
   for(uint8_t n=1;n<=N_NODES;n++) {
-    routes[n-1] = 0;
-    rssi[n-1] = 0;
+    RHRouter::RoutingTableEntry *route = manager->getRouteTo(n);
+    if (n == nodeId) {
+      routes[n-1] = 255; // self
+    } else {
+      routes[n-1] = route->next_hop;
+      if (routes[n-1] == 0) {
+        // if we have no route to the node, reset the received signal strength
+        rssi[n-1] = 0;
+      }
+    }
   }
 }
 
+// Create a JSON string with the routing info to each node
+void getRouteInfoString(char *p, size_t len) {
+  p[0] = '\0';
+  strcat(p, "[");
+  for(uint8_t n=1;n<=N_NODES;n++) {
+    strcat(p, "{\"n\":");
+    sprintf(p+strlen(p), "%d", routes[n-1]);
+    strcat(p, ",");
+    strcat(p, "\"r\":");
+    sprintf(p+strlen(p), "%d", rssi[n-1]);
+    strcat(p, "}");
+    if (n<N_NODES) {
+      strcat(p, ",");
+    }
+  }
+  strcat(p, "]");
+}
+
+void printNodeInfo(uint8_t node, char *s) {
+  Serial.print(F("node: "));
+  Serial.print(F("{"));
+  Serial.print(F("\""));
+  Serial.print(node);
+  Serial.print(F("\""));
+  Serial.print(F(": "));
+  Serial.print(s);
+  Serial.println(F("}"));
+}
+
 void loop() {
+
   for(uint8_t n=1;n<=N_NODES;n++) {
     if (n == nodeId) continue; // self
 
-    // Replace the routing info with your text message
-    strcpy(buf, "helo from node 1");
+    updateRoutingTable();
+    getRouteInfoString(buf, RH_MESH_MAX_MESSAGE_LEN);
 
     Serial.print(F("->"));
     Serial.print(n);
@@ -84,10 +158,13 @@ void loop() {
       Serial.println(getErrorString(error));
     } else {
       Serial.println(F(" OK"));
+      // we received an acknowledgement from the next hop for the node we tried to send to.
+      RHRouter::RoutingTableEntry *route = manager->getRouteTo(n);
+      if (route->next_hop != 0) {
+        rssi[route->next_hop-1] = rf95.lastRssi();
+      }
     }
-
-    // Print the routing table after each transmission
-    manager->printRoutingTable();
+    if (nodeId == 1) printNodeInfo(nodeId, buf); // debugging
 
     // listen for incoming messages. Wait a random amount of time before we transmit
     // again to the next node
@@ -102,7 +179,14 @@ void loop() {
         Serial.print(F("->"));
         Serial.print(F(" :"));
         Serial.println(buf);
+        if (nodeId == 1) printNodeInfo(from, buf); // debugging
+        // we received data from node 'from', but it may have actually come from an intermediate node
+        RHRouter::RoutingTableEntry *route = manager->getRouteTo(from);
+        if (route->next_hop != 0) {
+          rssi[route->next_hop-1] = rf95.lastRssi();
+        }
       }
     }
   }
+
 }
